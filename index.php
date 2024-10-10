214f15be387bbbcecb1669f0f975365dbba446bc
micheleboanno92 <michelebonanno92@gmail.com>
2024-10-10 17:15:24 +0200

    dichiarate varabili d'istanza nella classe

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 
     class Movie {
-        
+        public $name;
+        public $year;
+        public $type;
+
     }
+
+
+    $pino = new Movie();
+    $pino->name = 'Pino';
+    $pino->year = 1999;
+    $pino->type = 'comic';
+
+
+    var_dump($pino);
+
+
+
+
+
+
+
+
+
+
 
 ?>
