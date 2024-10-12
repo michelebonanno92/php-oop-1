6996f99b9d851b78175364959f23b7d5c6883001
micheleboanno92 <michelebonanno92@gmail.com>
2024-10-12 22:15:12 +0200

    aggiunto un costruttore

--- index.php
+++ index.php
@@ -10,6 +10,17 @@
         public $director;  // string
         public $duration;  // int
 
+        // aggiunto un costruttore per non stampare uno ad uno le proprietà 
+        function __construct($title,$year,$genres,$actors, $vote, $director, $duration  ) {
+            $this->title = $title;
+            $this->year = $year;
+            $this->genres = $genres;
+            $this->actors = $actors;
+            $this->vote = $vote;
+            $this->director = $director;
+            $this->duration = $duration;
+        }
+
         // aggiunta funzione getTitle per prendere il valore di title ora che è private
         public function getTitle() {
             return $this->title;
@@ -23,58 +34,76 @@
                 // lanciare un errore 
             }
         }
-
-
     }
 
-
-    $LUltimoDeiSamurai = new Movie();
-    $LUltimoDeiSamurai->setTitle("L'ultimo dei samurai");
-    $LUltimoDeiSamurai->year = 2003;
-    $LUltimoDeiSamurai->genres = [
-        'adventure',
-        'dramatic',
-        'historical',
-        'action',
-    ];
-    $LUltimoDeiSamurai->actors = [
-        'Tom Cruise',
-        'Ken Watanabe',
-        'Tony Goldwyn',
-        'Shichinosuke Nakamura',
-    ];
-    $LUltimoDeiSamurai->vote = 5;
-    $LUltimoDeiSamurai->director = 'Edward Zwick';
-    $LUltimoDeiSamurai->duration = 150;
+    $LUltimoDeiSamurai = new Movie(
+        "L'ultimo dei samurai",
+        2003,
+        [
+            'adventure',
+            'dramatic',
+            'historical',
+            'action',
+        ], 
+        [
+            'Tom Cruise',
+            'Ken Watanabe',
+            'Tony Goldwyn',
+            'Shichinosuke Nakamura',
+        ],
+        5,
+        'Edward Zwick',
+        150
+        );
 
     var_dump($LUltimoDeiSamurai);
     var_dump($LUltimoDeiSamurai->getTitle());
+    // $LUltimoDeiSamurai = new Movie();
+    // $LUltimoDeiSamurai->setTitle("L'ultimo dei samurai");
+    // $LUltimoDeiSamurai->year = 2003;
+    // $LUltimoDeiSamurai->genres = [
+    //     'adventure',
+    //     'dramatic',
+    //     'historical',
+    //     'action',
+    // ];
+    // $LUltimoDeiSamurai->actors = [
+    //     'Tom Cruise',
+    //     'Ken Watanabe',
+    //     'Tony Goldwyn',
+    //     'Shichinosuke Nakamura',
+    // ];
+    // $LUltimoDeiSamurai->vote = 5;
+    // $LUltimoDeiSamurai->director = 'Edward Zwick';
+    // $LUltimoDeiSamurai->duration = 150;
+
+
 
     
 
 
  
 
-    $Braveheart = new Movie();
-    $Braveheart->setTitle('Braveheart');
-    $Braveheart->year = 1995;
-    $Braveheart->genres = [
-        'epic',
-        'dramatic',
-        'historical',
-        'biographical',
-    ];
-    $Braveheart->actors = [
-        'Mel Gibson',
-        'Sophie Marceau',
-        'Brendan Gleeson',
-        'James Cosmo',
-    ];
-    $Braveheart->vote = 5;
-    $Braveheart->director = 'Mel Gibson';
-    $Braveheart->duration = 177;
+    // $Braveheart = new Movie();
+    // $Braveheart->setTitle('Braveheart');
+    // $Braveheart->year = 1995;
+    // $Braveheart->genres = [
+    //     'epic',
+    //     'dramatic',
+    //     'historical',
+    //     'biographical',
+    // ];
+    // $Braveheart->actors = [
+    //     'Mel Gibson',
+    //     'Sophie Marceau',
+    //     'Brendan Gleeson',
+    //     'James Cosmo',
+    // ];
+    // $Braveheart->vote = 5;
+    // $Braveheart->director = 'Mel Gibson';
+    // $Braveheart->duration = 177;
 
-    var_dump( $Braveheart);
+    // var_dump( $Braveheart);
 
 
 ?>
